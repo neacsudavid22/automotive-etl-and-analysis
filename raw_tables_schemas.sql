1b4e2668d7fcbfcce98c0619ec68bc1f7dd592d9
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-10-07 22:43:58 +0300

    added raw_vehicle_sales

---
+++ raw_tables_schemas.sql
@@ -0,0 +1,43 @@
+CREATE TABLE raw_vehicle_data (
+    v_id NUMBER,
+    constructor_name VARCHAR2(100),
+    vehicle_type VARCHAR2(50),
+    version VARCHAR2(50),
+    brand VARCHAR2(100),
+    vehicle_model VARCHAR2(100),
+    vehicle_category VARCHAR2(10),
+    kg_veh NUMBER,
+    test_mass NUMBER,
+    co2_wltp NUMBER,
+    wheelbase_mm NUMBER,
+    axle_width_steer_mm NUMBER,
+    axle_width_other_mm NUMBER,
+    energy VARCHAR2(50),
+    engine_cm3 NUMBER,
+    power_kw NUMBER,
+    el_consumpt_whkm NUMBER,
+    year NUMBER(4),
+    fuel_consumption NUMBER,
+    electric_range_km NUMBER,
+    eco_innovation_program NUMBER,
+    em_on_target NUMBER
+);
+
+CREATE TABLE raw_vehicle_sales (
+    company VARCHAR2(50),
+    model VARCHAR2(100),
+    model_type VARCHAR2(50),
+    car_size VARCHAR2(50),
+    transmission VARCHAR2(20),
+    car_state VARCHAR2(10),
+    car_condition VARCHAR2(10),
+    odometer NUMBER,
+    color VARCHAR2(30),
+    interior VARCHAR2(30),
+    seller VARCHAR2(100),
+    mmr NUMBER,
+    selling_price NUMBER,
+    sale_day VARCHAR2(10),
+    sale_month VARCHAR2(10),
+    sale_year NUMBER
+);
