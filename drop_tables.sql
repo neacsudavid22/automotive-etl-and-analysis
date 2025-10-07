efb65a838f19b698687dc416b5243fca8aa70785
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-10-07 22:43:33 +0300

    Create drop_tables.sql
    moved all drop statements in one place

---
+++ drop_tables.sql
@@ -0,0 +1,10 @@
+drop table raw_vehicle_data;
+drop table clean_vehicle_data;
+
+drop table raw_vehicle_sales;
+
+drop table vehicle_versions;
+drop table vehicle_models;
+drop table vehicle_companies;
+
+drop table vehicle_sales;
