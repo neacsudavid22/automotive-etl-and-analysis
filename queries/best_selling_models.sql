37f5f1ed4b5903a822731786966067fa551bf446
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-10-09 17:39:47 +0300

    Update best_selling_models.sql
    - the rank is always 1

--- queries/best_selling_models.sql
+++ queries/best_selling_models.sql
@@ -1,13 +1,13 @@
-SELECT DISTINCT *
+SELECT DISTINCT brand, model, total_sales
 FROM (
     SELECT 
-        c.brand,
-        m.model_name,
+        c.brand as brand,
+        m.model_name as model,
         SUM(s.selling_price) AS total_sales,
         RANK() OVER (PARTITION BY c.company_id ORDER BY SUM(s.selling_price) DESC) AS rang
     FROM vehicle_sales s
     JOIN vehicle_models m ON s.model_id = m.model_id
-    JOIN vehicle_companies c ON s.company_id = c.company_id
+    JOIN vehicle_companies c ON m.company_id = c.company_id
     GROUP BY c.brand, c.company_id, m.model_name
 )
 WHERE rang = 1
