1443a4e09c5c4a4c280595e6045e66de54c79c03
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-10-09 17:38:46 +0300

    Update drop_tables.sql

--- setup/drop_tables.sql
+++ setup/drop_tables.sql
@@ -3,8 +3,8 @@ drop table clean_vehicle_data;
 
 drop table raw_vehicle_sales;
 
+drop table vehicle_sales;
+
 drop table vehicle_versions;
 drop table vehicle_models;
 drop table vehicle_companies;
-
-drop table vehicle_sales;
